a4e4adbaea84f7764d1af425ead5a3f0bac4f521
EugenyX_X <workdjek@gmail.com>
2023-10-16 16:40:40 +0300

    Add errors exceptions

--- Scenes/Level/Level.gd
+++ Scenes/Level/Level.gd
@@ -34,7 +34,21 @@ func _process(delta):
 # Генерация матрицы для уровня levelSize: размер матрицы, 
 # maxPlate: максимальное количество полей
 # minPlate: минимальное количество полей
-func generateLevel(levelSize: int, minPlate: int, maxPlate: int) -> void:
+func generateLevel(levelSize: int, minPlate: int, maxPlate: int):
+	
+	if minPlate > maxPlate:
+		printerr("MinPlate не может быть больше MaxPlate ИДИОТ")
+		return null
+	if maxPlate >= (levelSize * levelSize ):
+		printerr("MaxPlate не может быть больше или равен размеру матрицы ИДИОТ")
+		return null
+	if levelSize <= 1 || levelSize >= 5: 
+		printerr("Размер матрицы за переделами значений 2 - 4 ИДИОТ")
+		return null
+	if maxPlate <= 0 || minPlate <= 0:
+		printerr("MaxPlate или MinPlate не может быть отрицательным числом или нулем ИДИОТ")
+		return null
+	
 	var sumPlate = 0
 	var matrixTemplate: Array
 	
@@ -50,15 +64,16 @@ func generateLevel(levelSize: int, minPlate: int, maxPlate: int) -> void:
 	
 	for x in matrixTemplate.size():
 		for y in matrixTemplate[x].size():
-			if randi_range(0, 1) == 1 && sumPlate <= maxPlate:
+			if randi_range(0, 1) == 1 && sumPlate < maxPlate:
 				matrixTemplate[x][y] = 1
 				sumPlate += 1
 	
 	while sumPlate < minPlate:
 		sumPlate = 0
+		matrixTemplate.clear()
 		for x in matrixTemplate.size():
 			for y in matrixTemplate[x].size():
-				if randi_range(0, 1) == 1 && sumPlate <= maxPlate:
+				if randi_range(0, 1) == 1 && sumPlate < maxPlate:
 					matrixTemplate[x][y] = 1
 					sumPlate += 1
 	
