92d3f2df4bd9a265b8edb25a404d4fc0adf2ee6a
bl4ckFrisk <glofeed9@gmail.com>
2023-10-23 21:03:37 +0300

    Added protected plates

---
+++ Scenes/Plates/ProtectedPlate.gd
@@ -0,0 +1,50 @@
+extends Control
+
+@onready var plate = $ColorRect
+var switch = false
+@export var health = 3
+
+func checkHp(hp: int):
+	var buttonInfo = self.name.split('_', true, 3)
+	if hp == 0:
+		if switch:
+			plate.set_color(Color(1,1,1))
+			switch = false
+			GeneratorLevel.updateMatrix(int(buttonInfo[1]), int(buttonInfo[2]), 0)
+		else:
+			plate.set_color(Color(0,0,0))
+			switch = true
+			GeneratorLevel.updateMatrix(int(buttonInfo[1]), int(buttonInfo[2]), 1)
+		print("Player Matrix", PlayerStatus.getPlayerLevelField())
+		print("Matrix 1", PlayerStatus.getCurrentLevelField(0))
+		print("Matrix 2", PlayerStatus.getCurrentLevelField(1))
+		if PlayerStatus.setCompareMatrix(GeneratorLevel.compareMatrix(PlayerStatus.getCurrentLevelField(0),PlayerStatus.getPlayerLevelField())):
+			print('Compare 1')
+			PlayerStatus.setCurrentLevelField()
+		elif PlayerStatus.setCompareMatrix(GeneratorLevel.compareMatrix(PlayerStatus.getCurrentLevelField(1),PlayerStatus.getPlayerLevelField())):
+			print("Compare 2")
+			PlayerStatus.setCurrentLevelField()
+
+func _on_on_click_pressed():
+	if health > 0:
+		health -= 1
+	checkHp(health)
+#	var buttonInfo = self.name.split('_', true, 3)
+	
+#	if switch:
+#		plate.set_color(Color(1,1,1))
+#		switch = false
+#		GeneratorLevel.updateMatrix(int(buttonInfo[1]), int(buttonInfo[2]), 0)
+#	else:
+#		plate.set_color(Color(0,0,0))
+#		switch = true
+#		GeneratorLevel.updateMatrix(int(buttonInfo[1]), int(buttonInfo[2]), 1)
+#	print("Player Matrix", PlayerStatus.getPlayerLevelField())
+#	print("Matrix 1", PlayerStatus.getCurrentLevelField(0))
+#	print("Matrix 2", PlayerStatus.getCurrentLevelField(1))
+#	if PlayerStatus.setCompareMatrix(GeneratorLevel.compareMatrix(PlayerStatus.getCurrentLevelField(0),PlayerStatus.getPlayerLevelField())):
+#		print('Compare 1')
+#		PlayerStatus.setCurrentLevelField()
+#	elif PlayerStatus.setCompareMatrix(GeneratorLevel.compareMatrix(PlayerStatus.getCurrentLevelField(1),PlayerStatus.getPlayerLevelField())):
+#		print("Compare 2")
+#		PlayerStatus.setCurrentLevelField()
